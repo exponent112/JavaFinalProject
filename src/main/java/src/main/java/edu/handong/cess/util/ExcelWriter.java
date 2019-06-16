e0687d3101167830cfca5e6d03d8040f7dd4f075
exponent112 <21800181@handong.edu>
2019-06-16 22:06:12 +0900

    second

--- src/main/java/src/main/java/edu/handong/cess/util/ExcelWriter.java
+++ src/main/java/src/main/java/edu/handong/cess/util/ExcelWriter.java
@@ -24,15 +24,36 @@ public class ExcelWriter {
 				BufferedWriter writer = new BufferedWriter(
 						new OutputStreamWriter(outputStream));
 				String[] out;
-				for(int i=0;i<rows;i++) {
+				writer.write("Student number");
+				writer.write(",");
+				writer.write("제목");
+				writer.write(",");
+				writer.write("요약문 (300자 내외)");
+				writer.write(",");
+				writer.write("\"핵심어\n" + 
+						"(keyword,쉽표로 구분)\"");
+				writer.write(",");
+				writer.write("조회날짜");
+				writer.write(",");
+				writer.write("\"실제자료조회\n" + 
+						"출처 (웹자료링크)\"");
+				writer.write(",");
+				writer.write("원출처 (기관명 등)");
+				writer.write(",");
+				writer.write("\"제작자\n" + 
+						"(Copyright 소유처)\"");
+				writer.write("\n");
+				System.out.println("");
+				for(int i=1;i<rows;i++) {
 					
 					writer.write("000"+str);
 					writer.write(",");
 					for(int j=0;j<7;j++) {
-						while(values.get(n).equals("\n")) n++;
+						//while(values.get(n).equals("\n")) n++;
 						writer.write(values.get(n));
 						writer.write(",");
 						n++;
+						if(n>=35) break;
 					}
 					writer.write("\n");
 				}
@@ -44,17 +65,28 @@ public class ExcelWriter {
 			}//catch
 		}//if(a = 1)
 		else {
-			int n=4;
+			int n=5;
 			try {
 				String rename = outpath.replace(".", "2.");
 				outputStream = new FileOutputStream(rename);
 				BufferedWriter writer = new BufferedWriter(
 						new OutputStreamWriter(outputStream));
 				String[] out;
+				writer.write("Student number");
+				writer.write(",");
+				writer.write("제목(반드시 요약문 양식에 입력한 제목과 같아야 함.)");
+				writer.write(",");
+				writer.write("표/그림 일련번호");
+				writer.write(",");
+				writer.write("\"자료유형(표,"+"그림"+","+"…)\"");
+				writer.write(",");
+				writer.write("자료에 나온 표나 그림 설명(캡션)");
+				writer.write(",");
+				writer.write("자료가 나온 쪽번호");
+				writer.write("\n");
 				for(int i=0;i<rows;i++) {
 					writer.write("000"+str);
 					writer.write(",");
-					
 					for(int j=0;j<5;j++) {
 						writer.write(values.get(n));
 						writer.write(",");
